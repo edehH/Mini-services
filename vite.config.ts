9f2e57cc32d7851dadfe57213a0bde248232e5fd
edehH <edehwww@gmail.com>
2026-07-22 21:34:27 +0000

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,25 @@
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      base: './',
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      build: {
-        target: 'es2020',
-      },
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+    plugins: [
+      tailwindcss(),
+      react(),
+    ],
+    build: {
+      target: ['chrome60', 'firefox60', 'safari11', 'edge18', 'es2015'],
+      cssTarget: ['chrome60', 'firefox60', 'safari11', 'edge18'],
+    },
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY || ''),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY || '')
+    }
+  };
 });
